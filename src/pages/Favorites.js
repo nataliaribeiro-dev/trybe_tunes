500885bfe0c3130aaf909b8b21d4dd1b3414fbd1
Natalia Ribeiro <natilgr@hotmail.com>
2023-05-16 15:08:26 -0300

    feat(1): implement routes

---
+++ src/pages/Favorites.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Favorites extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-favorites" />
+
+    );
+  }
+}
+
+export default Favorites;
---
+++ src/pages/ProfileEdit.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class ProfileEdit extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-profile-edit" />
+
+    );
+  }
+}
+
+export default ProfileEdit;
